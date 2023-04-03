faa0c22152f1fb564278fd2d1657a31688e7e0e3
Meaneac <Kyle@schang.net>
2023-04-03 17:23:00 -0400

    added comments to script

--- scripts/update_yaml.bash
+++ scripts/update_yaml.bash
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+# change directory to workflows
 cd workflows/
+
+# add temp file
 touch temp.txt
+
+# add pre-commit-config.txt to temp file
 cat ~/Dropbox/Programming/dotfiles/.pre-commit-config.txt >> temp.txt
+
+# remove pre-commit-config.yaml
 rm .pre-commit-config.yaml
+
+# rename temp file to pre-commit-config.yaml
 mv temp.txt .pre-commit-config.yaml
+
+# change permissions to be executable
 chmod +x .pre-commit-config.yaml
