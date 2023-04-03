5edb9cedd3fe2d0e49555cd6a0cc203558011ec3
Meaneac <Kyle@schang.net>
2023-04-03 17:20:56 -0400

    fixes

--- scripts/update_yaml.bash
+++ scripts/update_yaml.bash
@@ -2,7 +2,7 @@
 
 cd workflows/
 touch temp.txt
-cat ../.pre-commit-config.txt >> temp.txt
+cat ~/Dropbox/Programming/dotfiles/.pre-commit-config.txt >> temp.txt
 rm .pre-commit-config.yaml
 mv temp.txt .pre-commit-config.yaml
 chmod +x .pre-commit-config.yaml
